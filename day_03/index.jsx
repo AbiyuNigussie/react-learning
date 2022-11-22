f84eb1f296562d60e75253b9d3c4c43f673c5b5f
AbiyuTolosa <abiyunigussie7@gmail.com>
2022-11-22 17:58:41 +0300

    index.js deleted!

--- day_03/index.jsx
+++
@@ -1,9 +0,0 @@
-const header = (
-  <header>
-    <h1>Welcome to 30 Days of React</h1>
-    <h2>Getting Started React</h2>
-    <h3>JavaScript Library</h3>
-    <p>Abiyu Nigussie</p>
-    <small>Nov 22, 2022</small>
-  </header>
-);
